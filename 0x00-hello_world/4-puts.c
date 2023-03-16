5eb508a33206829d5b731bde905f5eff374f004b
gabitech07 <gabitech11@gmail.com>
2023-03-16 04:42:26 -0700

    c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description - the program prints a string
+ * return - always(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}	
