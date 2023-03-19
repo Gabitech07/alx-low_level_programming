c9ff42c05f705c221a8079f302292302174e56f8
gabitech07 <gabitech11@gmail.com>
2023-03-19 12:25:18 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int c;
+	int b;
+
+	for (c = 97; c <= 122; c++)
+	{
+		putchar(c);
+	}
+	for (b = 65; b <= 90; b++)
+	{
+		putchar(b);
+	}
+	putchar('\n');
+	return (0);
+}
