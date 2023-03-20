de65f2257877bb3a9c31be21d3c9c9dee7e75c76
gabitech07 <gabitech11@gmail.com>
2023-03-20 14:21:57 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ * Description: Get a random number and check its last digit, compare it with 5
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = 122; a >= 65; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
